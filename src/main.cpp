59dd7e82e58765552596ec16bedbd7eb2e7976ce
MatthewAlbisser <144923804+MatthewAlbisser@users.noreply.github.com>
2024-10-17 21:34:32 -0400

    Commits retrieved from the warp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "raylib.h"
 #include "Math.h"
-#include <thread>   // Included after looking for a way to hold .
+#include <thread>   // Included after looking for a way to hold.
 
 constexpr float SCREEN_WIDTH = 1200.0f;
 constexpr float SCREEN_HEIGHT = 800.0f;
@@ -65,9 +65,9 @@ void ResetBall(Vector2& position, Vector2& direction)
     position = CENTER;
     direction.x = rand() % 2 == 0 ? -1.0f : 1.0f;
     direction.y = 0.0f;
-    direction = Rotate(direction, Random(0.0f, 60.0f) * DEG2RAD);   // Changed float 360 to 60; dont need another direction flip.
-}                                                                   // fixes stuck-in-center bug; ball has no X-axis movement.
-
+    direction = Rotate(direction, Random(0.0f, 60.0f) * DEG2RAD);   // [Secondary Choice Feature] Changed float 360 to 60.
+}                                                                   // We dont need another random direction flip.
+                                                                    // Fixes bug were ball has no X-axis movement.
 void DrawBall(Vector2 position, Color color)
 {
     Box ballBox = BallBox(position);
@@ -91,8 +91,19 @@ int main()
     paddle2Position.x = SCREEN_WIDTH * 0.95f;
     paddle1Position.y = paddle2Position.y = CENTER.y;
 
-    static int player1Points = 0;  // Local variable for player 1 points.
-    static int player2Points = 0;  // Local variable for player 2 points.
+    static int player1Points = 0;   // Local variable for player 1 points.
+    static int player2Points = 0;   // Local variable for player 2 points.
+
+    static int volley = 0;      // [Main Choice Feature] Variable for holding volleys between players.
+    bool showVolley = false;    // [Main Choice Feature] Sets bool for volley count.
+
+    InitAudioDevice();                                      // Creates audio device.
+    Sound sfx1 = LoadSound("ping_pong_8bit_beeep.wav");     // cache sound file to be played later.
+    Sound sfx2 = LoadSound("ping_pong_8bit_peeeeeep.wav");  
+    Sound sfx3 = LoadSound("ping_pong_8bit_plop.wav");      
+    SetSoundVolume(sfx1, 0.1f);                             // Set volume to 10% of source value.
+    SetSoundVolume(sfx2, 0.1f);                             
+    SetSoundVolume(sfx3, 0.1f);                             
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong");
     SetTargetFPS(60);
@@ -107,13 +118,13 @@ int main()
             paddle1Position.y -= paddleDelta;
         if (IsKeyDown(KEY_S))
             paddle1Position.y += paddleDelta;
-        if (IsKeyDown(KEY_E))                   // Second player controls, because its much more fun with TWO players. 
-            paddle2Position.y -= paddleDelta;
+        if (IsKeyDown(KEY_E))                   // [Secondary Choice Feature] Second player controls.
+            paddle2Position.y -= paddleDelta;   // Because its much more fun with TWO players.
         if (IsKeyDown(KEY_D))
             paddle2Position.y += paddleDelta;
 
         // Mirror paddle 1 for now
-        // paddle2Position.y = paddle1Position.y;   // Replaced for second player controls
+        // paddle2Position.y = paddle1Position.y;   // [Secondary Choice Feature] Replaced for second player controls
 
         float phh = PADDLE_HEIGHT * 0.5f;
         paddle1Position.y = Clamp(paddle1Position.y, phh, SCREEN_HEIGHT - phh);
@@ -126,51 +137,69 @@ int main()
         Box paddle2Box = PaddleBox(paddle2Position);
 
         if (ballBox.xMin < 0.0f || ballBox.xMax > SCREEN_WIDTH)
-            // ballDirection.x *= -1.0f;            // Commented out so ball reset function can work without issue. 
-            ResetBall(ballPosition, ballDirection); // Calls the ball reset function using local Vectors.
+            ballDirection.x *= -1.0f,                
+            ResetBall(ballPosition, ballDirection);     // Calls the ball reset function using local Vectors.
 
-        if (ResetBall && ballBox.xMax > SCREEN_WIDTH)   // If ball is on the right when ball resets...
-            player1Points += 1,                         // 1 point to player 1.
-            paddle1Position.y = CENTER.y,               // Paddle 1 resets position.
-            paddle2Position.y = CENTER.y;               // Paddle 2 resets position.
-        if (ResetBall && ballBox.xMin < 0.0f)           // If ball is on the left when ball resets...
-            player2Points += 1,                         // 1 point to player 2.
-            paddle1Position.y = CENTER.y,               // Paddle 1 resets position.
-            paddle2Position.y = CENTER.y;               // Paddle 2 resets position.
+        if (ResetBall && ballBox.xMax > SCREEN_WIDTH)               // If ball is on the right when ball resets...
+            PlaySound(sfx3),                                        // Play low sfx.
+            player1Points += 1,                                     // 1 point to player 1.
+            volley = 0,                                             // [Main Choice Feature] Resets volley count to 0.
+            paddle1Position.y = CENTER.y,                           // Paddle 1 resets position.
+            paddle2Position.y = CENTER.y;                           // Paddle 2 resets position.
 
-        if (player1Points == 5)
-            DrawText("Player One Wins!", 250, 250, 100, LIME),
-            player1Points = 0,
-            player2Points = 0,
-            EndDrawing(),
-            std::this_thread::sleep_for(std::chrono::seconds(3)),
-            exit(0);
-        if (player2Points == 5)
-            DrawText("Player Two Wins!", 250, 250, 100, LIME),
-            player1Points = 0,
-            player2Points = 0,
-            EndDrawing(),
-            std::this_thread::sleep_for(std::chrono::seconds(3)),
-            exit(0);
+        if (ResetBall && ballBox.xMin < 0.0f)                       // If ball is on the left when ball resets...
+            PlaySound(sfx3),                                        // Play low sfx.
+            player2Points += 1,                                     // 1 point to player 2.
+            volley = 0,                                             // [Main Choice Feature] Resets volley count to 0.
+            paddle1Position.y = CENTER.y,                           // Paddle 1 resets position.
+            paddle2Position.y = CENTER.y;                           // Paddle 2 resets position.
+
+        if (player1Points == 5)                                     // If player 1 reaches 5 points...
+            DrawText("Player One Wins!", 175, 250, 100, LIME),      // End game text center screen.
+            player1Points = 0,                                      // Reset player 1 points to 0. (not neccessary when function also exits the program)
+            player2Points = 0,                                      // Reset player 2 points to 0. (not neccessary when function also exits the program)
+            PlaySound(sfx2),                                        // Play long beep sfx.
+            EndDrawing(),                                           // Used to wait 1 tick so text can be drawn.
+            std::this_thread::sleep_for(std::chrono::seconds(3)),   // Holds code for 3 seconds. 
+            exit(0);                                                // Exits game.
+
+        if (player2Points == 5)                                     // If player 1 reaches 5 points...
+            DrawText("Player Two Wins!", 175, 250, 100, RED),       // End game text center screen.
+            player1Points = 0,                                      // Reset player 1 points to 0.
+            player2Points = 0,                                      // Reset player 2 points to 0.
+            PlaySound(sfx2),                                        // Play long beep sfx.
+            EndDrawing(),                                           // Used to wait 1 tick so text can be drawn.
+            std::this_thread::sleep_for(std::chrono::seconds(3)),   // Holds code for 3 seconds.
+            exit(0);                                                // Exits game.
 
         if (ballBox.yMin < 0.0f || ballBox.yMax > SCREEN_HEIGHT)
             ballDirection.y *= -1.0f;
         if (BoxOverlap(ballBox, paddle1Box) || BoxOverlap(ballBox, paddle2Box))
+            volley++,
+            PlaySound(sfx1),                                                            // Play Sfx when ball hits paddle.
             ballDirection.x *= -1.0f;
+
+        if (volley == 5 || volley == 10)                                                // [Main Choice Feature] When volley count is 5 or 10...
+            showVolley = true;                                                          // [Main Choice Feature] Set show volley count to true.
+
+        if (showVolley == true)                                                         // [Main Choice Feature] If volley count set to true...
+            DrawText(TextFormat("%i Volleys!", volley), 500, 200, 50, SKYBLUE);         // [Main Choice Feature] Show text for number of volleys.
+
+        if (volley >= 5 || volley <= 5 || volley >= 10 || volley <= 10)                 // [Main Choice Feature] If volley count is less then or greater then 5 or 10...
+            showVolley = false;                                                         // [Main Choice Feature] Set show volley count to false.
 
         // Update ball position after collision resolution, then render
         ballPosition = ballPosition + ballDirection * ballDelta;
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText(TextFormat("Player One: %i", player1Points), 20, 10, 20, GRAY);
-        DrawText(TextFormat("Player Two: %i", player2Points), 1050, 10, 20, GRAY);
+        DrawText(TextFormat("Player One: %i", player1Points), 20, 10, 20, GRAY);        // Draw score text for player 1 per tick.
+        DrawText(TextFormat("Player Two: %i", player2Points), 1050, 10, 20, GRAY);      // Draw score text for player 2 per tick.
         DrawBall(ballPosition, WHITE);
         DrawPaddle(paddle1Position, WHITE);
         DrawPaddle(paddle2Position, WHITE);
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
